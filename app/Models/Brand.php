35696365c2d24af01f8184dc60f51730ff2ff3af
Tisha Mehta <mehtatisha35@gmail.com>
2024-06-19 18:56:20 +0530

    First Commit basic work done

---
+++ app/Models/Brand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    protected $fillable=['title','slug','status'];
+
+   
+    public function products(){
+        return $this->hasMany('App\Models\Product','brand_id','id')->where('status','active');
+    }
+    public static function getProductByBrand($slug){
+        return Brand::with('products')->where('slug',$slug)->first();
+    }}
